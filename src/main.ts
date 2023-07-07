f791bd6663a971d9e34bb6dc78c6d8793bdc8263
Simón A Azócar <saimn.azocar@gmail.com>
2023-07-06 21:54:56 -0400

    feat: :construction: add swagger config in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('School API')
+    .setDescription('This API was made with educational purpose')
+    .setVersion('1.0')
+    .addTag('Person')
+    .addTag('School')
+    .addTag('Section')
+    .addTag('Faculty')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
